9ba203db3397f9ef9068378b5abc72d009919cf3
m2nseop <pms246@naver.com>
2021-11-27 11:22:21 +0900

    feat:ImageButton,findViewById for Button,TextView
    1. ImageButton을 통해 정답입력 버튼 추가했다.
    2. 숫자Button과 숫자Button을 눌렀을때 출력되는 textView의 객체를
    onCreate 메소드에 findViewById 메소드를 통해 받도록하였다.

--- app/src/main/java/com/example/numberbaseball/MainActivity.java
+++ app/src/main/java/com/example/numberbaseball/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import java.util.Random;
 
@@ -12,11 +13,21 @@ public class MainActivity extends AppCompatActivity {
 
     int[] comNumbers = new int[3]; // 3개의 랜덤한 숫자 저장용 배열
 
+    TextView[] inputTextView = new TextView[3]; // 내가 선택한 숫자 저장
+    Button[] numButton = new Button[10]; // 내가 누른 숫자 버튼
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         comNumbers = getComNumbers(); // 여기서 생성하고 저장한다.
+        for (int i = 0; i < inputTextView.length; i++) {
+            inputTextView[i] = findViewById(R.id.input_text_view_0 + i);
+        }
+        for (int i = 0; i < numButton.length; i++) {
+            numButton[i] = findViewById(R.id.num_button_0 + i);
+        }
+        
     }
 
     public int[] getComNumbers() {
