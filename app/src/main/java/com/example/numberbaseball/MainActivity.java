f76dbc56b46a266ac28855ef48e50d24fb0084fd
m2nseop <pms246@naver.com>
2021-12-17 16:04:57 +0900

    feat:selector,onClick-backSpaceButton,hitButton
    selector for different color of Button by each condition.
    onClick for backSpaceButton that change info about pressed button.
    method getCountCheck and onClick for hitButton that makes result info
    about answer.
    onClick for Button for print Text, change info about pressed button.

--- app/src/main/java/com/example/numberbaseball/MainActivity.java
+++ app/src/main/java/com/example/numberbaseball/MainActivity.java
@@ -4,23 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    int[] comNumbers = new int[3]; // 3개의 랜덤한 숫자 저장용 배열
+    int[] comNumbers = new int[3]; // 3개의 랜덤한 숫자(=정답) 저장용 배열
+    int inputTextCount = 0; // 버튼을 눌렸을때 누른 버튼 개수 카운트 용, 숫자버튼을 3개 누른후에 더 이상 누르지 못하게 한다.
 
-    TextView[] inputTextView = new TextView[3]; // 내가 선택한 숫자 저장
+    TextView[] inputTextView = new TextView[3]; // 내가 선택한 숫자 저장, 야구공 출력용
     Button[] numButton = new Button[10]; // 내가 누른 숫자 버튼
+
+    ImageButton backSpaceButton;
+    ImageButton hitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        comNumbers = getComNumbers(); // 여기서 생성하고 저장한다.
+        setContentView(R.layout.activity_main); // setContentView 메소드가 xml 에 있는 뷰들을 메모리상에 객체화시킨다.
+
+        comNumbers = getComNumbers(); // 정답 숫자 랜덤 생성후 comNumbers 에 저장
+
         for (int i = 0; i < inputTextView.length; i++) {
             inputTextView[i] = findViewById(R.id.input_text_view_0 + i);
         }
@@ -28,8 +37,94 @@ public class MainActivity extends AppCompatActivity {
             numButton[i] = findViewById(R.id.num_button_0 + i);
         }
 
+        backSpaceButton = findViewById(R.id.backspace_button);
+        hitButton = findViewById(R.id.hit_button);
+
+        // 숫자버튼을 클릭했을때 야구공모양에 해당 숫자 출력되게 하기 + 중복,개수초과 방지
+        for(Button getNumButton : numButton){ // getNumButton 으로 numButton 배열을 순차적으로 받고
+            getNumButton.setOnClickListener(new View.OnClickListener() { // 이것을 통해
+                @Override
+                public void onClick(View v) {
+                    if (inputTextCount < 3) {
+                        // 버튼이 클릭되었을때 어떻게 될지 구현하는 부분
+                        Button button = findViewById(v.getId());
+                        // findViewById로 선택한 버튼을 받아와서 변수 button 에 저장한다., R.id가 아니라 여기서는 View v 로 받기 때문에 v.getId() 로 받아온다.
+                    /*  String getButtonNumber = button.getText().toString();
+                    *   inputTextView[inputTextCount].setText(getButtonNumber); */
+                        // 위에 두줄을 간단하게 하면 아래
+                        inputTextView[inputTextCount].setText(button.getText().toString()); // 선택한 숫자 버튼에 대한 숫자 텍스트를 inputTextView 의 inputTextCount 번째에 저장한다.
+                        button.setEnabled(false); // 중복된 숫자 선택 방지용으로 버튼 비활성화
+                        inputTextCount++; // 선택한 숫자 개수 카운트
+                    }
+                    else {
+                        Toast.makeText(getApplicationContext(), "정답입력 버튼을 눌러주세요", Toast.LENGTH_SHORT).show();
+                    // Toast 는 하단에 작은 텍스트 메세지를 잠시 보여주는 이벤트이다.
+                    }
+                }
+            });
+        }
+
+        // 지우기 버튼 OnClick 이벤트 설정
+        backSpaceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (inputTextCount > 0) {
+                    int buttonEnableCount = Integer.parseInt(inputTextView[inputTextCount - 1].getText().toString()); // 좀 전에 누른 숫자버튼의 숫자의 텍스트(문자열)를 받아와 int 로 바꿈
+                    numButton[buttonEnableCount].setEnabled(true); // 해당버튼을 enable true 로 바꿈
+                    inputTextView[inputTextCount - 1].setText(""); // ""를 통해 야구공의 텍스트 비워줌
+                    // inputTextCount-1 인 이유는 위의 for 문에서 inputTextCount 가 마지막에 1이 또 추가된상황이기 때문에 -1 을 빼줘야 인덱스 2부터 접근한다.
+                    inputTextCount--;
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "숫자를 입력해 주세요", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        // 정답입력버튼 OnClick 이벤트 설정
+        hitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (inputTextCount < 3){
+                    Toast.makeText(getApplicationContext(), "숫자를 입력해 주세요", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    int[] userNumbers = new int[3]; // 내가 누른 버튼의 숫자를 저장할 배열
+                    for (int i = 0; i < userNumbers.length ; i++) {
+                        userNumbers[i] = Integer.parseInt(inputTextView[i].getText().toString());
+                    }
+                    int[] countCheck; // [0]은 strike [1]은 ball 카운트
+                    countCheck = getCountCheck(comNumbers, userNumbers);
+                    Log.e("hitButton", "countCheck = S : " + countCheck[0] + " B : " + countCheck[1]); // 확인용 로그
+
+                    inputTextCount = 0; // 선택한 숫자 카운트 초기
+                    for (TextView textView : inputTextView) {
+                        textView.setText(""); // 선택한 숫자 전부 텍스트 지움
+                    }
+                    for (Button button : numButton){
+                        button.setEnabled(true); // 선택한 숫자 버튼 전부 활성화
+                    }
+                }
+            }
+        });
+
+    }
+    // 정답 체크용 메서드, comNumbers 는 정답 배열, userNumbers 는 내가 누른 숫자 배열
+    private int[] getCountCheck(int[] comNumbers, int[] userNumbers) {
+        int[] setCount = new int[2]; // 스트라잌과 볼을 저장할 2칸짜리 int 형 배열
+        for (int i = 0; i < comNumbers.length; i++) {
+            for (int j = 0; j < userNumbers.length; j++) {
+                if ((comNumbers[i] == userNumbers[j]) && (i == j)){ // 숫자와 자리가 같으면
+                    setCount[0]++; // 스트롸잌
+                }else if((comNumbers[i] == userNumbers[j]) && (i != j)) { // 숫자만 같고 자리가 다르면
+                    setCount[1]++; // 볼
+                }
+            }
+        }
+        return setCount;
     }
 
+    // 랜덤의 3개의 숫자인 정답을 생성하는 메소드
     public int[] getComNumbers() {
         int[] setComNumbers = new int[3];
         for (int i = 0; i < setComNumbers.length; i++) {
